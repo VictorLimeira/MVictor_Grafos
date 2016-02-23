cd033e32c560d4659097dd288b2fd6b2b1bde589
Mannuel Victor Limeira <victor.limeira@outlook.com>
2016-02-23 16:53:23 -0300

    Adicionado apresentacao de matriz de adjacencia

--- grafo.py
+++ grafo.py
@@ -6,7 +6,7 @@ IFRN - Natal/RN - Brazil
 2016
 Contact: victor.limeira@outlook.com
 """
-
+from __future__ import print_function
 
 class VerticeExiste(Exception):
     pass
@@ -41,13 +41,13 @@ class Grafo():
 
         nova_aresta = Aresta(valor, saida, entrada)
 
-        if not self.arestas:
-            self.arestas.append(nova_aresta)
-        else:
+        if self.arestas:
             for aresta in self.arestas:
                 if [aresta.valor, aresta.saida, aresta.entrada] == [
                         valor, saida, entrada]:
                     raise ArestaExiste()
+            self.arestas.append(nova_aresta)
+        else:
             self.arestas.append(nova_aresta)
 
     def mostrar_vertices(self):
@@ -56,7 +56,46 @@ class Grafo():
         valores = []
         for vertice in self.vertices:
             valores.append(vertice.valor)
-        print valores
+        print(valores)
+
+    def mostrar_arestas(self):
+        """ Mostrar todos os valores das arestas inseridas """
+
+        valores = []
+        for aresta in self.arestas:
+            valores.append([aresta.valor, aresta.saida, aresta.entrada])
+        print(valores)
+
+    def matriz_adjacencia(self):
+        """ Mostra a matriz de adjacencia do Grafo """
+        # Primeira linha
+        print("   ", end="")
+        for vertice in self.vertices:
+            print(" " + str(vertice.valor) + " ", end="")
+        print("")
+        # Fim primeira linha
+
+        encontrado = False
+        for vertice in self.vertices:
+            # Inicio de cada linha
+            print(" " + str(vertice.valor) + " ", end="") # Imprime o valor do vertice atual na horizontal
+            # Comparacao de cada outro vertice com o atual
+            for vertice2 in self.vertices:
+                # Varredura das arestas
+                for aresta in self.arestas:
+                    valores = [aresta.saida, aresta.entrada]
+                    # Encontrar se existe aresta para os dois vertices
+                    if ((vertice.valor is valores[0]) and (vertice2.valor is valores[1])
+                        ) or ((vertice.valor is valores[1]) and (vertice2.valor is valores[0])): # Aresta encontrada
+                        print(" " + str(1) + " ", end="")
+                        encontrado = True
+                # Apos a verradura de todas as arestas, se nao foi encontrado, colocar 0.
+                if encontrado is not True:
+                    print(" " + str(0) + " ", end="")
+                else:
+                    encontrado = False
+            print("") # Fim de cada linha
+
 
 
 class Vertice():
--- main.py
+++ main.py
@@ -10,8 +10,14 @@ from grafo import *
 
 if __name__ == '__main__':
 
-    meu_grafo = Grafo(2)
+    # Popular o grafo
+    meu_grafo = Grafo(2) # Inicia o grafo já com o vertice 2
     meu_grafo.inserir_vertice(3)
-    meu_grafo.mostrar_vertices()
+    meu_grafo.inserir_vertice(4)
+    meu_grafo.inserir_vertice(5)
     meu_grafo.inserir_aresta(3, 2, 3)
-    meu_grafo.inserir_aresta(3, 2, 3)
+    meu_grafo.inserir_aresta(4, 4, 2)
+    meu_grafo.inserir_aresta(8, 5, 4)
+
+    # Imprimir matriz de adjacencia
+    meu_grafo.matriz_adjacencia()
